c6de2b84196017333d9169fe15b41a5ccf4cc678
a9sk <920a9sk42f76c765@proton.me>
2024-10-02 23:17:59 +0200

    added dependencies.go initial code

---
+++ dependencies/dependencies.go
@@ -0,0 +1,67 @@
+package dependencies
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func checkAndInstallDependencies() {
+	fmt.Println("Checking for required dependencies...")
+
+	if !isCommandAvailable("ffmpeg") {
+		fmt.Println("ffmpeg is not installed.")
+		installTool("ffmpeg")
+	} else {
+		fmt.Println("ffmpeg is installed.")
+	}
+
+	if !isCommandAvailable("tesseract") {
+		fmt.Println("Tesseract is not installed.")
+		installTool("tesseract")
+	} else {
+		fmt.Println("Tesseract is installed.")
+	}
+}
+
+func isCommandAvailable(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
+
+func installTool(toolName string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Do you want to install %s? (yes/no): ", toolName)
+	response, _ := reader.ReadString('\n')
+	response = strings.TrimSpace(strings.ToLower(response))
+
+	if response == "yes" || response == "y" {
+		switch toolName {
+		case "ffmpeg":
+			installFFmpeg()
+		case "tesseract":
+			installTesseract()
+		default:
+			fmt.Printf("Unknown tool: %s\n", toolName)
+		}
+	} else {
+		log.Fatalf("%s is required. Exiting...\n", toolName)
+	}
+}
+
+func detectOS() string {
+	switch os := runtime.GOOS; os {
+	case "linux":
+		return "linux"
+	case "darwin":
+		return "darwin"
+	case "windows":
+		return "windows"
+	default:
+		return "unknown"
+	}
+}

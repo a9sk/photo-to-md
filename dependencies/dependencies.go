27cdcad5ac0a90f25b5a297fc71faf959d002c79
a9sk <920a9sk42f76c765@proton.me>
2024-10-05 00:34:52 +0200

    added dependencies installation

--- dependencies/dependencies.go
+++ dependencies/dependencies.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-func checkAndInstallDependencies() {
+func CheckAndInstallDependencies() {
 	fmt.Println("Checking for required dependencies...")
 
 	if !isCommandAvailable("ffmpeg") {

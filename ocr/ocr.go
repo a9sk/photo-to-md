75fa779854e2c32a31d55531215ec2c6112c54f4
a9sk <920a9sk42f76c765@proton.me>
2024-10-08 22:59:55 +0200

    split ocr and hocr

--- ocr/ocr.go
+++ ocr/ocr.go
@@ -2,11 +2,55 @@ package ocr
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 
-func PerformOCR(imagePath string) (string, error) {
+type StyledText struct {
+	Text  string
+	Style string
+}
+
+func PerformOCR(imagePath string) ([]StyledText, error) {
+	fmt.Println("Using Tesseract to retrive text from the image")
+
+	isSpecific := os.Getenv("ISSPECIFIC")
+
+	var styledOutput []StyledText
+
+	if isSpecific == "true" {
+		output, err := doOCR(imagePath)
+		if err != nil {
+			var emptyRet []StyledText
+			emptyRet = append(emptyRet, StyledText{
+				Text:  "",
+				Style: "",
+			})
+			return emptyRet, fmt.Errorf("error running the OCR: %v", err)
+		}
+		styledOutput = append(styledOutput, StyledText{
+			Text:  output,
+			Style: "",
+		})
+
+	} else {
+		var err error
+		styledOutput, err = dohOCR(imagePath)
+		if err != nil {
+			var emptyRet []StyledText
+			emptyRet = append(emptyRet, StyledText{
+				Text:  "",
+				Style: "",
+			})
+			return emptyRet, fmt.Errorf("error running the hOCR: %v", err)
+		}
+	}
+
+	return styledOutput, nil
+}
+
+func doOCR(imagePath string) (string, error) {
 	fmt.Println("Performing OCR on image...")
 
 	cmd := exec.Command("tesseract", imagePath, "stdout")
@@ -16,4 +60,23 @@ func PerformOCR(imagePath string) (string, error) {
 	}
 
 	return strings.TrimSpace(string(output)), nil
+}
+
+func dohOCR(imagePath string) ([]StyledText, error) {
+	fmt.Println("Performing hOCR on image...")
+
+	cmd := exec.Command("tesseract", imagePath, "stdout", "--hocr")
+	output, err := cmd.Output()
+	if err != nil {
+
+		var emptyRet []StyledText
+		emptyRet = append(emptyRet, StyledText{
+			Text:  "",
+			Style: "",
+		})
+
+		return emptyRet, fmt.Errorf("error running Tesseract: %v", err)
+	}
+
+	return parseHOCR(string(output)), nil
 }

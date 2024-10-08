3d0dd050cb1e6d59b1a469c5edc2e6294fe9fac4
a9sk <920a9sk42f76c765@proton.me>
2024-10-08 19:28:53 +0200

    fixed path issues

--- save/tomd.go
+++ save/tomd.go
@@ -3,14 +3,13 @@ package save
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"time"
 )
 
 func SaveMarkdown(text string) error {
 	markdownContent := fmt.Sprintf("# Extracted Text - %s\n\n%s", time.Now().Format(time.RFC1123), text)
 
-	outputPath := filepath.Join(os.TempDir(), "output.md")
+	outputPath := "output.md"
 	err := os.WriteFile(outputPath, []byte(markdownContent), 0644)
 	if err != nil {
 		return fmt.Errorf("error saving markdown file: %v", err)

e21576736fc4dd46a6159850441ec8ae41527114
a9sk <920a9sk42f76c765@proton.me>
2024-10-08 23:01:24 +0200

    added specific out conditions

--- save/tomd.go
+++ save/tomd.go
@@ -6,8 +6,22 @@ import (
 	"time"
 )
 
-func SaveMarkdown(text string) error {
-	markdownContent := fmt.Sprintf("# Extracted Text - %s\n\n%s", time.Now().Format(time.RFC1123), text)
+type StyledText struct {
+	Text  string
+	Style string
+}
+
+func SaveMarkdown(text []StyledText) error {
+
+	var stringText string
+
+	if os.Getenv("ISSPECIFIC") == "true" {
+		stringText = htmlToMarkdown(text)
+	} else {
+		stringText = text[0].Text
+	}
+
+	markdownContent := fmt.Sprintf("# Extracted Text - %s\n\n%s", time.Now().Format(time.RFC1123), stringText)
 
 	outputPath := "output.md"
 	err := os.WriteFile(outputPath, []byte(markdownContent), 0644)

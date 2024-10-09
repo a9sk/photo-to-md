a23cc92193a859cae5d9775be8c05d271a732f19
a9sk <920a9sk42f76c765@proton.me>
2024-10-09 17:15:45 +0200

    added extract bbox and extract xml

--- ocr/extract.go
+++ ocr/extract.go
@@ -1,6 +1,7 @@
 package ocr
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -11,4 +12,26 @@ func extractText(line string, startTag string, endTag string) string {
 		return line[start+len(startTag) : end]
 	}
 	return ""
+}
+
+type BBox struct {
+	width  int
+	height int
+}
+
+func extractBBox(line string) BBox {
+	var x1, y1, x2, y2 int
+	if _, err := fmt.Sscanf(line, `bbox %d %d %d %d`, &x1, &y1, &x2, &y2); err != nil {
+		return BBox{0, 0}
+	}
+	return BBox{
+		width:  x2 - x1,
+		height: y2 - y1, //normal 25, usage 32, title 40
+	}
+}
+
+func extractXML(hocrHTML string) string {
+	var sb strings.Builder
+	sb.WriteString(extractText(hocrHTML, "<body>", "</body>"))
+	return sb.String()
 }
